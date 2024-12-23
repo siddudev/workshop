a9ac971ecd9d00a0aedde6a66222f82204693e50
DebashishMahato198 <devmahato198@gmail.com>
2024-12-23 16:05:45 +0530

    added for loop'

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ public class Sample{
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("Piyush ");
+        System.out.println("Devashish");
     }
 }
