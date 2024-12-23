cb02e3789efd7eb59523800ea315b0786ff06cf6
navneet64bit <navneetkumar1849@gmail.com>
2024-12-23 16:05:59 +0530

    Changes in pyush name

--- Sample.java
+++ Sample.java
@@ -1,7 +1,7 @@
 public class Sample{
     public static void main(String[] args){
         System.out.println("hello world");
-        System.out.println("Piyush ");
+        System.out.println("Navneet Kumar");
         System.out.println("github");
         System.out.println("merge");
     }
